dd575205cddcf31b426931b1fce466ed021bc9a3
MrFerhan <ferhkem@gmail.com>
2022-06-15 13:56:56 -0700

    edit

--- _realloc.c
+++ _realloc.c
@@ -39,6 +39,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (i = 0; i < old_size && i < new_size; i++)
 		*((char *)p + i) = *((char *)ptr + i);
 	free(ptr); /* free old ptr */
-
 	return (p);
 }
